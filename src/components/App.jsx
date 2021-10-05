b313371c882c519ad887f4f7ad03bd49a08d0548
stevenpfaff <pfaffy1426@gmail.com>
2021-10-05 16:35:24 -0500

    added footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import TitleBar from './TitleBar/TitleBar';
 import BookViewer from './BookViewer/BookViewer';
 import BookCreator from './BookCreator/BookCreator';
+import Footer from './Footer/Footer';
 
 class App extends Component {
     constructor(props){
@@ -43,7 +44,7 @@ goToPreviousBook = () => {
 
 createBook = (newBook) => {
     this.books.push(newBook);
-    
+
 }
 
 render() {
@@ -52,6 +53,7 @@ render() {
             <TitleBar />
             <BookViewer book={this.books[this.state.bookNumber]} nextBook={this.goToNextBook} previousBook={this.goToPreviousBook} />
             <BookCreator createNewBook={this.createBook}/>
+            <Footer />
         </div>
         )
     }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <h4> Copywright @ 2021 </h4>
+        </footer>
+    )
+}

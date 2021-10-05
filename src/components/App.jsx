359873933e4a475b2b12b6222edcf6f1c63fdd79
stevenpfaff <pfaffy1426@gmail.com>
2021-10-05 14:16:57 -0500

    created new app component + created components folder + created titlebar component and CSS file

---
+++ src/components/App.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './App.css'
+import TitleBar from './TitleBar/TitleBar';
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.books = [
+            { title: "Ready Player One", author: "Ernest Cline" },
+            { title: "All the Light We Cannot See", author: "Anthony Doerr" },
+            { title: "The First and Last Freedom", author: "Jiddu Krishnamurit" }
+        ];
+        this.state = {
+            bookNumber: 0
+        };
+    }
+
+render() {
+    return(
+        <div className="container-fluid">
+            <TitleBar />
+            <div className="row">
+                <div className="col-md-4">
+                </div>
+                <div className="col-md-4">
+                    <h1>{this.books[this.state.bookNumber].title}</h1>
+                    <h4>{this.books[this.state.bookNumber].author}</h4>
+                </div>
+                <div className="col-md-4">
+                </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default App;
